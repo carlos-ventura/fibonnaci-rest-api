3025536530957bd3e7a6c17c09d653e0a2cb61da
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-08 14:17:06 +0100

    Add functionality and return to fibonacci list API Get call with pagination support

--- project/app/main.py
+++ project/app/main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
-from app.tasks import fibonacci_task
+from fastapi_pagination import Page as BasePage, Params, paginate, add_pagination
+
+from app.tasks import fibonacci_task, fibonacci_task_pairs
+from app.models import FibonacciPair
 
 app = FastAPI()
 
+Page = BasePage.with_custom_options(size=100)
 
 @app.get("/fibonacci/")
 async def fibonacci(number: int):
     fibonacci_number = await fibonacci_task(number)
     return { "fibonacci_number" : fibonacci_number }
 
-@app.get("/fibonacci/list")
+@app.get("/fibonacci/list", response_model=Page[FibonacciPair])
 async def fibonacci_list(number: int):
-    return
+    fibonacci_pairs = await fibonacci_task_pairs(number)
+    return paginate(fibonacci_pairs)
 
 @app.post("/fibonacci/blacklist")
 async def add_to_blacklist(number: int):
@@ -20,3 +25,5 @@ async def add_to_blacklist(number: int):
 @app.delete("/fibonacci/delete_from_blacklist")
 async def delete_from_blacklist(number: int):
     return
+
+add_pagination(app)
