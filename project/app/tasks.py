b8a803383a6eae56c01bcae0099ccada3e12d244
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-09 17:42:55 +0100

    Add alternative fibonacci functions

--- project/app/tasks.py
+++ project/app/tasks.py
@@ -15,6 +15,13 @@ async def fibonacci(number: int) -> int:
     return number if number < 2 else await fibonacci(number - 1) + await fibonacci(number - 2)
 
 
+async def fibonacci_v2(number: int) -> int:
+    start_n, next_n = 0, 1
+    for _ in range(number):
+        start_n, next_n = next_n, next_n+start_n
+    return start_n
+
+
 async def fibonacci_task(number: int) -> int:
     handle_input(number)
     blacklist = [el.number for el in await fibonacci_blacklist_task()]
@@ -39,6 +46,22 @@ async def fibonacci_task_pairs(number: int) -> List[FibonacciPair]:
     ]
 
 
+async def fibonacci_task_pairs_v2(number: int) -> List[FibonacciPair]:
+    handle_input(number=number, zero_allowed=False)
+    fibonacci_list: List[FibonacciPair] = []
+    blacklist = [el.number for el in await fibonacci_blacklist_task()]
+
+    if len(blacklist) >= number:
+        raise HTTPException(
+            status_code=404, detail="All numbers are blacklisted")
+
+    start_n, next_n = 1, 1
+    for i in range(number):
+        if i + 1 not in blacklist:
+            fibonacci_list.append(FibonacciPair(
+                number=i + 1, fibonacci_number=start_n))
+        start_n, next_n = next_n, next_n+start_n
+    return fibonacci_list
 
 
 async def add_to_blacklist_task(number: int):
