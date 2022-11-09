7ad3da230df9c2bc5a52fbb2f9dfea8d38e48771
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-09 14:03:39 +0100

    Add DAL method to add entried in blacklist table

--- project/app/db/dals/blacklist_dal.py
+++ project/app/db/dals/blacklist_dal.py
@@ -1,5 +1,16 @@
+import sqlalchemy
 from sqlalchemy.orm import Session
+
+from app.db.models.blacklist import Blacklist
 
 class BlacklistDAL():
     def __init__(self, db_session: Session):
         self.db_session = db_session
+
+    async def add_blacklist_number(self, number: int):
+        blacklist_number = Blacklist(number = number)
+        self.db_session.add(blacklist_number)
+        try:
+            await self.db_session.commit()
+        except sqlalchemy.exc.IntegrityError:
+            pass
