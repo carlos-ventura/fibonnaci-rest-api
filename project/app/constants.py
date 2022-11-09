24bf80f14878d7623101647a761ba4f6df6cdd7f
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-09 18:54:49 +0100

    Refactor description of API

---
+++ project/app/constants.py
@@ -0,0 +1,16 @@
+DESCRIPTION = """
+   Fibonacci API
+
+   ## Items
+
+   * **Fibonacci** - Returns the value from the Fibonacci sequence for a given number.
+
+   * **Fibonacci List** - Returns a list of numbers and the corresponding values from
+   the Fibonacci sequence from 1 to N
+
+   * **Add To Blacklist** - Adds a number to the blacklist 
+   (numbers not allowed to calculate the value from the Fibonacci sequence)
+
+   * **Delete From Blacklist** - Deletes a number from the blacklist
+
+   """
--- project/app/main.py
+++ project/app/main.py
@@ -10,25 +10,11 @@ from app.tasks import (
 )
 from app.models import FibonacciPair
 from app.db.config import Base, engine
-
-description = """
-   Fibonacci API
-
-   ## Items
-
-   * **Fibonacci** - Returns the value from the Fibonacci sequence for a given number.
-
-   * **Fibonacci List** - Returns a list of numbers and the corresponding values from the Fibonacci sequence from 1 to N
-
-   * **Add To Blacklist** - Adds a number to the blacklist (numbers not allowed to calculate the value from the Fibonacci sequence)
-
-   * **Delete From Blacklist** - Deletes a number from the blacklist
-
-   """
+from app.constants import DESCRIPTION
 
 app = FastAPI(
     title="Fibonacci",
-    description=description,
+    description=DESCRIPTION,
 )
 
 Page = BasePage.with_custom_options(size=100)
