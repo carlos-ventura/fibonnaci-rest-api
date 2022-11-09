a02e20fbc98d6559ee19675e2ff64222e2b202db
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-09 14:24:47 +0100

    Create add to blacklist task

--- project/app/tasks.py
+++ project/app/tasks.py
@@ -2,6 +2,9 @@ import asyncio
 from typing import List
 
 from app.models import FibonacciPair
+from app.utils import handle_input
+from app.db.dals.blacklist_dal import BlacklistDAL
+from app.db.config import async_session
 
 async def fibonacci_task(number: int) -> int:
     return number if number < 2 else await fibonacci_task(number - 1) + await fibonacci_task(number - 2)
@@ -9,3 +12,11 @@ async def fibonacci_task(number: int) -> int:
 async def fibonacci_task_pairs(number: int) -> List[FibonacciPair]:
     results = await asyncio.gather(*[fibonacci_task(n) for n in range(1, number + 1)])
     return [ FibonacciPair(number=i+1, fibonacci_number=results[i]) for i in range(len(results))]
+
+async def add_to_blacklist_task(number: int):
+    handle_input(number)
+    async with async_session() as session:
+        async with session.begin():
+            blacklist_dal = BlacklistDAL(session)
+            await blacklist_dal.add_blacklist_number(number)
+
