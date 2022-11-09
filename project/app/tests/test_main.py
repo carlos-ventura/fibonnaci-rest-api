a0f4186a23957594b058a76378a6e061d89a4cbf
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-09 22:31:25 +0100

    Extend tests

--- project/app/tests/test_main.py
+++ project/app/tests/test_main.py
@@ -28,19 +28,35 @@ app.dependency_overrides[get_session] = override_get_session
 
 @pytest.mark.asyncio
 async def test_fibonacci():
-
     async with AsyncClient(app=app, base_url="http://test") as async_client:
         response = await async_client.get("/fibonacci/", params={"number": 13})
-    assert response.status_code == 200
-    assert response.json() == fibonacci_13
+        assert response.status_code == 200
+        assert response.json() == fibonacci_13
+
+        response = await async_client.get("/fibonacci/", params={"number": -10})
+        assert response.status_code == 400
 
 
 @pytest.mark.asyncio
 async def test_fibonacci_list():
     async with AsyncClient(app=app, base_url="http://test") as async_client:
-        response = await async_client.get("/fibonacci/list", params={"number": 5})
-    assert response.status_code == 200
-    assert response.json() == fibonacci_list_5
+        search_number_object = {"number": 5}
+        deleted_number_object = {"number": 2, "fibonacci_number": 1}
+        response = await async_client.get("/fibonacci/list", params=search_number_object)
+        assert response.status_code == 200
+        assert response.json() == fibonacci_list_5
+
+        response = await async_client.post(
+            "/fibonacci/add_to_blacklist", params={"number": deleted_number_object["number"]}
+        )
+        assert response.status_code == 200
+
+        response = await async_client.get("/fibonacci/list", params=search_number_object)
+        assert response.status_code == 200
+        assert deleted_number_object not in response.json()['items']
+
+        response = await async_client.get("/fibonacci/list", params={"number": 0})
+        assert response.status_code == 400
 
 
 @pytest.mark.asyncio
@@ -62,9 +78,8 @@ async def test_add_to_blacklist():
 
         response = await async_client.get("/fibonacci/blacklist")
         assert response.status_code == 200
-        assert len(response.json()) == 2
-        assert all(value in [add_number_object, add_number_object2]
-                   for value in response.json())
+        assert all(value in response.json()
+                   for value in [add_number_object, add_number_object2])
 
 
 @pytest.mark.asyncio
