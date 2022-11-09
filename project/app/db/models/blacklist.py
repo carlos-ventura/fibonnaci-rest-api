d82aac8c234c8c98302404377ed8337f6fbb0a77
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-09 13:58:59 +0100

    Create structure for the database table

---
+++ project/app/db/models/blacklist.py
@@ -0,0 +1,9 @@
+
+from sqlalchemy import Column, Integer
+
+from app.db.config import Base
+
+class Blacklist(Base):
+    __tablename__ = 'blacklist'
+
+    number = Column(Integer, primary_key=True)
