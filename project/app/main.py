73f4f763eee51fbd67d16a50bdd95b38bfe0609d
carlos-ventura <carlospinho_pt@hotmail.com>
2022-11-10 00:13:22 +0100

    Switch fibonacci functions

--- project/app/main.py
+++ project/app/main.py
@@ -2,8 +2,8 @@ from app.constants import DESCRIPTION
 from app.db.config import Base, async_session, engine
 from app.models import FibonacciPair
 from app.tasks import (add_to_blacklist_task, delete_from_blacklist_task,
-                       fibonacci_blacklist_task, fibonacci_task,
-                       fibonacci_task_pairs)
+                       fibonacci_blacklist_task, fibonacci_task_pairs_v2,
+                       fibonacci_task_v2)
 from fastapi import Depends, FastAPI
 from fastapi_pagination import Page as BasePage
 from fastapi_pagination import add_pagination, paginate
@@ -34,13 +34,13 @@ async def startup():
 
 @app.get("/fibonacci/")
 async def fibonacci(number: int, session: Session = Depends(get_session)):
-    fibonacci_number = await fibonacci_task(number, session)
+    fibonacci_number = await fibonacci_task_v2(number, session)
     return {"fibonacci_number": fibonacci_number}
 
 
 @app.get("/fibonacci/list", response_model=Page[FibonacciPair])
 async def fibonacci_list(number: int,  session: Session = Depends(get_session)):
-    fibonacci_pairs = await fibonacci_task_pairs(number, session)
+    fibonacci_pairs = await fibonacci_task_pairs_v2(number, session)
     return paginate(fibonacci_pairs)
 
 
